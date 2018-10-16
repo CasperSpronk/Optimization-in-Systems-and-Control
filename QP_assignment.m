e76431b41a9c235f9c096013ddba233522405cfd
CSpronk <casperspronk@live.nl>
2018-10-16 12:44:49 +0200

    QP setup

---
+++ QP_assignment.m
@@ -0,0 +1,15 @@
+% Made by: 
+% Sven Geboers 4439686
+% Casper Spronk 4369475
+%% Setup
+clc
+clear all
+D1sven = 6;
+D2sven = 8;
+D3sven = 6;
+D1casper = 4;
+D2casper = 7;
+D3casper = 5;
+E1 = D1sven + D1casper;
+E2 = D2sven + D2casper;
+E3 = D3sven + D3casper;
