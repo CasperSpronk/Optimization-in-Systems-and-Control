866bef42b5bbe3aeccbcfe7eb5b4783130e7a91e
CSpronk <casperspronk@live.nl>
2018-10-16 16:54:01 +0200

    question 3

--- QP_assignment.m
+++ QP_assignment.m
@@ -14,15 +14,13 @@ E1 = D1sven + D1casper;
 E2 = D2sven + D2casper;
 E3 = D3sven + D3casper;
 %% Read CSV
-heatDemand = csvread('heatDemand.csv',1,1);
-inputPrices = csvread('inputPrices.csv',1,1);
 measurements = csvread('measurements.csv',1,1);
 Qin = measurements(:,1);
 Qout = measurements(:,2);
 T = measurements(:,3);
 Tamb = measurements(:,4);
 %% Question 1
-t_delta = 3600;
+tDelta = 3600;
 %B = [-a2 a2];
 %A = -a1;
 %ck = Tamb * a1;
@@ -53,6 +51,38 @@ end
 x = linspace(0,1,150);
 y = linspace(0,1,111);
 plot(x,T,y,Tnew)
+
+%% Question 3
+heatDemand = csvread('heatDemand.csv',1,1);
+inputPrices = csvread('inputPrices.csv',1,1);
+N = 360;
+QinMax = (100 + E2) * 10^3;         % [W]
+Ttank = 330 + E3;                      % [K]
+Tamb = 275 + E1;                    % [K]   
+Tmin = 315;                         % [K]
+a1 = 1.96 * 10^-7;
+a2 = 3.80 * 10^-9;
+correctPriceVar = 60 * 10^6;
+inputPrices = inputPrices * correctPriceVar;
+
+for i = 1:N
+    f = [inputPrices(i)*tDelta 0];
+    A = [1 0;
+         -1 0
+         0 -1];
+    b = [QinMax;
+        0;
+        Tmin];  
+    Aeq = [a2 a1];
+    
+    
+    
+    linprog(f,A,b,Aeq,beq)
+end
+
+
+
+
 
 
 
