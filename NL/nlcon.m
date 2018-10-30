4358dcf5fcfd6bd2ce3ba70394f36f13d4b7ee64
CSpronk <casperspronk@live.nl>
2018-10-30 13:31:43 +0100

    changed ceq

--- NL/nlcon.m
+++ NL/nlcon.m
@@ -4,7 +4,7 @@ function [c,ceq] = nlcon(x)
         for j = 1:9
             xPlus1 = f(x(1:9,i),x(10:13,i));
             ceqHold = xPlus1 - x(1:9,i+1);
-            ceq(1:9,i) = ceqHold;
+            ceq(1:9,i+1) = ceqHold;
         end
     end
     c = [];
