c90011b53834647fa686377e0bacf8b387176e1a
CSpronk <casperspronk@live.nl>
2018-10-30 13:06:16 +0100

    question 2 complete

--- NL/nlcon.m
+++ NL/nlcon.m
@@ -1,7 +1,10 @@
 function [c,ceq] = nlcon(x)
+    ceq = zeros(9,59);
     for i = 1:59
         for j = 1:9
-            ceq = f(x(1:9,i),x(10:13,i)) - x(j,i+1);
+            xPlus1 = f(x(1:9,i),x(10:13,i));
+            ceqHold = xPlus1 - x(1:9,i+1);
+            ceq(1:9,i) = ceqHold;
         end
     end
     c = [];
